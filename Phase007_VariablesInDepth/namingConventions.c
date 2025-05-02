76523299dc30c53662ffdbf6cfdd6dddf7ce1078
Deepak Ghimire <codeepakghimire@gmail.com>
2025-05-02 08:25:07 +0545

    Updated

---
+++ Phase007_VariablesInDepth/namingConventions.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main(){
+    // rules
+    // 1. can't start with digit ex: int 2globe = 2;
+    // 2. void use of underscore _
+    // 3. case sensative, var and Var is different
+    // 4. special characters (@.#,%,&,*) not allowed
+    // 5. white spaces are not allowed
+    // 6. defult/reserved keywords like(if,else) are not allowed
+    // 7. do not use long names
+    return 0;
+
+    
+    
+}
---
+++ Phase007_VariablesInDepth/varDepth.c
@@ -0,0 +1,30 @@
+
+#include<stdio.h>
+
+int main(){
+    // int a = 20;
+    // variable is a container for storing values
+    // in C there are different datatypes defined with different keywords and stored in variable
+
+    int num; //decliration of variable
+    
+    // points:
+    // variable are names that points to some memory location like (int a = 20) it will create a memory of a 4 bytes
+    // the datatype given in variable defines the size of memory a variable will going to take
+    // semiclon (;) is a way to seperate a lines of code for compiler
+
+    int num2 = 20;//initialization of variable
+
+    // variable = vary-able 
+
+    // points:
+    // each variable only be defined once and can be used and change multiple time
+    int a, b, c, d;  // declare variables
+    a = b = c = d = 20;  // assign 20 to all
+    printf("%d\n", a);  // print the value of 'a'
+    return 0;
+
+    // for detailed visulation of c in memory visit:
+    // https://pythontutor.com/visualize.html#mode=edit
+
+}
